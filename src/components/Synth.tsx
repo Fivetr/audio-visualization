52c054a88651e6b648414aa31db162dedffd6b81
unknown <soslidesong@gmail.com>
2023-09-26 09:20:53 -0500

    Fixed start time must be strictly greater than previous start time

--- src/components/Synth.tsx
+++ src/components/Synth.tsx
@@ -4,85 +4,107 @@ import { Signal } from "tone";
 import AmpSimple from "./modules/amps/AmpSimple";
 import TriggerSwitch from "./modules/controlers/TriggerSwitch";
 import EnvelopeASDR from "./modules/envelope_generators/EnvelopeADSR";
-import LowpassFilter from "./modules/filters/lowpassFilter";
+import LowpassFilter from "./modules/filters/LowpassFilter";
 import OscSimple from "./modules/oscillators/OscSimple";
 import Rack from "./Rack";
 
 const Synth = () => {
-    const [ignored, setIgnored] = useState(0);
-    const forceUpdate = () => setIgnored(ignored+1);
-    
-    const [gain] = useState(new Signal(0.5));
-    const [oscillator] = useState(new Tone.Oscillator("C4", "sine"))
-    const [envelope] = useState(new Tone.AmplitudeEnvelope({
-        attack: 1, 
-        decay: 1, 
-        sustain: 0.5, 
-        release: 1.5
-    }));
-    const [amp] = useState(new Tone.Multiply());
-    const [filter] = useState(new Tone.Filter(22000, "lowpass", -12))
+  const [ignored, setIgnored] = useState(0);
+  const forceUpdate = () => setIgnored(ignored + 1);
 
-    useEffect(() => {
-        console.log(filter.Q.value)
-        filter.Q.value = 0;
-        console.log(filter.Q.value)
-        forceUpdate();
-    }, [])
+  const [gain] = useState(new Signal(0.5));
+  const [oscillator] = useState(new Tone.Oscillator("C4", "sine"));
+  const [envelope] = useState(
+    new Tone.AmplitudeEnvelope({
+      attack: 1,
+      decay: 1,
+      sustain: 0.5,
+      release: 1.5,
+    })
+  );
+  const [amp] = useState(new Tone.Multiply());
+  const [filter] = useState(new Tone.Filter(22000, "lowpass", -12));
 
+  useEffect(() => {
+    // console.log(filter.Q.value);
+    filter.Q.value = 0;
+    // console.log(filter.Q.value);
+    // forceUpdate();
     oscillator.chain(envelope, amp, filter, Tone.Destination);
-    gain.connect(amp.factor); 
+    gain.connect(amp.factor);
     oscillator.start();
-    const triggerAttack = () => envelope.triggerAttack();   
-    const triggerRelease = () => envelope.triggerRelease()
+    return () => {
+      oscillator.stop();
+      oscillator.dispose();
+      envelope.dispose();
+      amp.dispose();
+      filter.dispose();
+    };
+  }, []);
 
-    return (
-        <>
-            <Rack>
-                <TriggerSwitch 
-                    onHandler={triggerAttack}
-                    offHandler={triggerRelease}
-                    frequency={oscillator.frequency.value as number}
-                    setFrequency={(freq) => {
-                        oscillator.frequency.value = freq;
-                        forceUpdate();
-                    }}
-                />
-                <OscSimple setWaveform={(newWaveform: Tone.ToneOscillatorType) => oscillator.type = newWaveform}/>
-                <EnvelopeASDR 
-                    adsr={{
-                        attack: envelope.attack as number, 
-                        decay: envelope.decay as number, 
-                        sustain: envelope.sustain as number, 
-                        release: envelope.release as number, 
-                    }} 
-                    setAdsr={newAdsr => {
-                        envelope.attack = newAdsr.attack;
-                        envelope.decay = newAdsr.decay;
-                        envelope.sustain = newAdsr.sustain;
-                        envelope.release = newAdsr.release;
-                        forceUpdate();
-                    }
-                }/>
-                <LowpassFilter 
-                    cutoffFrequency={filter.frequency.value as number} 
-                    setCutoff={(freq) =>  {
-                        filter.frequency.value = freq;
-                        forceUpdate()
-                    }} 
-                    resonance={filter.Q.value} 
-                    setResonance={(q) => {
-                        filter.Q.value = q
-                        forceUpdate()
-                    }}
-                />
-                <AmpSimple setGain={(newGain: number) => {
-                    gain.value = newGain
-                    forceUpdate();
-                }} gain={gain.value}/>
-            </Rack>
-        </>
-    )
-}
+  const triggerRelease = () => envelope.triggerRelease();
+  const triggerAttack = () => {
+    envelope.triggerRelease();
+
+    setTimeout(() => {
+      envelope.triggerAttack();
+    }, 100);
+  };
+
+  return (
+    <>
+      <Rack>
+        <TriggerSwitch
+          onHandler={triggerAttack}
+          offHandler={triggerRelease}
+          frequency={oscillator.frequency.value as number}
+          setFrequency={(freq) => {
+            oscillator.frequency.value = freq;
+            forceUpdate();
+          }}
+        />
+        <OscSimple
+          setWaveform={(newWaveform: Tone.ToneOscillatorType) =>
+            (oscillator.type = newWaveform)
+          }
+        />
+        <EnvelopeASDR
+          adsr={{
+            attack: envelope.attack as number,
+            decay: envelope.decay as number,
+            sustain: envelope.sustain as number,
+            release: envelope.release as number,
+          }}
+          setAdsr={(newAdsr) => {
+            envelope.attack = newAdsr.attack;
+            envelope.decay = newAdsr.decay;
+            envelope.sustain = newAdsr.sustain;
+            envelope.release = newAdsr.release;
+            forceUpdate();
+          }}
+        />
+        <LowpassFilter
+          cutoffFrequency={filter.frequency.value as number}
+          setCutoff={(freq) => {
+            filter.frequency.value = freq;
+            forceUpdate();
+          }}
+          resonance={filter.Q.value}
+          setResonance={(q) => {
+            filter.Q.value = q;
+            forceUpdate();
+          }}
+        />
+        <AmpSimple
+          setGain={(newGain: number) => {
+            gain.value = newGain;
+            forceUpdate();
+          }}
+          gain={gain.value}
+        />
+      </Rack>
+    </>
+  );
+};
 
 export default Synth;
